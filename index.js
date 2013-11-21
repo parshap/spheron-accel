9419ce43f636f5109c7283b3f98c8b8e12e77148
Parsha Pourkhomami <parshap+git@gmail.com>
2013-11-21 02:37:17 -0800

    Use stream interface

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+var through = require("through");
+
 // Mask for accelerometer x y z data
 var MASK = 0x8000 | 0x4000 | 0x2000;
 
 module.exports = function(sphero, options) {
-	var rate = options.rate || 40, // default 10Hz
-		samples = options.samples || 1; // default 1 sample per packet
+	options = options || {};
+	var stream = through();
+	var rate = 400 / (options.rate || 10); // default 10Hz
 
-	// Turn on data streaming
-	sphero.setDataStreaming(rate, samples, MASK, 0);
+	// Turn on accelerometer data streaming
+	sphero.setDataStreaming(rate, 1, MASK, 0);
 
+	// Read incoming accelerometer data and emit on stream
 	sphero.on("packet", function(packet) {
 		if (packet.ID_CODE === 0x03) {
-			sphero.emit("accel", {
+			stream.queue({
 				x: packet.DATA.readInt16BE(0),
 				y: packet.DATA.readInt16BE(2),
 				z: packet.DATA.readInt16BE(4),
 			});
 		}
 	});
+
+	return stream;
 };
